90a9739fe8e22359f1294280531a5604d741e5b2
Hecker chuoi <nguyentienht1910@gmail.com>
2025-03-04 00:38:10 +0700

    init

---
+++ src/main/java/com/hecker/exam/MultipleChoiceExamApplication.java
@@ -0,0 +1,13 @@
+package com.hecker.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleChoiceExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleChoiceExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hecker/exam/MultipleChoiceExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hecker.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleChoiceExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
